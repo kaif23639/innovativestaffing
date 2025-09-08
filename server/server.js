3743812a9dea928f2c3ba686ce6218ddd934804a
kaif23639 <inovativestaffingsolutions.777@gmail.com>
2025-09-08 23:42:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    'https://innovativestaffing.vercel.app/',
+    'https://innovativestaffing.vercel.app',
     'https://innovativestaffing-xl5b.vercel.app',
     'http://localhost:5173', 
     'http://localhost:5174'
