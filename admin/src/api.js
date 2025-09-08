626b2e4b62c68a1c6aa4f80eb5926657d125e06c
kaif23639 <inovativestaffingsolutions.777@gmail.com>
2025-09-09 00:11:16 +0530

    Update api.js

--- admin/src/api.js
+++ admin/src/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
-const BASE_URL = 'https://innovative-staffing-1.onrender.com';
+const BASE_URL = 'https://innovativestaffing.onrender.com';
 const api = axios.create({
 
-  baseURL: 'https://innovative-staffing-1.onrender.com', // Your backend URL
+  baseURL: 'https://innovativestaffing.onrender.com', 
 
-  baseURL: BASE_URL, // Your backend URL
-
+  baseURL: BASE_URL, 
 });
 
-// Attach JWT token to each request if present
+
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('adminToken');
   if (token) {
