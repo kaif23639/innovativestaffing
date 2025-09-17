9fbbf0a5c0cab5774830b54a54ef3d52366e15db
kaif23639 <inovativestaffingsolutions.777@gmail.com>
2025-09-17 13:22:19 +0530

    Update userAuth.js

--- server/routes/userAuth.js
+++ server/routes/userAuth.js
@@ -6,6 +6,8 @@ const userController = require('../controllers/userController');
 
 router.post('/signup/request-otp', userController.requestSignupOtp);
 router.post('/signup/verify-otp', userController.verifySignupOtp);
+router.post('/password-reset/request',userController.requestPasswordReset);
+router.post('/password-reset/reset', userController.resetPassword);
 router.post('/login', login);
 
 
